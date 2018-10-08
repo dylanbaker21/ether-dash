24e43c989b0da21aeb843603d48c1a91af9f3f97
dylanbaker21 <dylan.j.baker@hotmail.com>
2018-10-07 20:10:24 -0400

    added contract source code viewing functionality

--- index.js
+++ index.js
@@ -6,23 +6,39 @@ window.onload= function() {
     selector: '.background',
     color: '#808080',
     connectParticles: true,
-    maxParticles: 115
+    maxParticles: 125
   });
 };
 
-
-var SRC_ADD = placeholder/*user input contract adress here*/
+var input
+var inputValue
+var FULL_SRC_CODE
 const SRC_CODE_URL = "https://api.etherscan.io/api?module=contract&action=getsourcecode&address="
 const TOKEN = "&apikey=U4WZMWZGNXEKF64XTG8YISH1B62BV95MY6"
 
-const FULL_SRC_CODE = SRC_CODE_URL + SRC_ADD + TOKEN
+function setURL(inputValue){
+    FULL_SRC_CODE = SRC_CODE_URL + inputValue + TOKEN
+}
 
-function request() {
+function displaySourceCode() {
+    let form = document.querySelector("form")
+    input = document.querySelector('#source-code-box')
+    form.addEventListener("submit", event => {
+        event.preventDefault()})
+        inputValue = input.value
+        setURL(inputValue)
+        requestSourceCode()
+        setTimeout(function(){ alert("Source code will open in a new tab, you may have to enable pop-ups")})
+}
+
+function requestSourceCode() {
     fetch(FULL_SRC_CODE)
     .then(function(result) {
         return result.json()
     })
     .then(function(res) {
+        let newWindow = window.open()
+        newWindow.document.write(res.result[0].SourceCode)
         console.log(res.result[0].SourceCode)
     })
     .catch(function(error) {
